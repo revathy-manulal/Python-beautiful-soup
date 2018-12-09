341bd5b4d1676282532d3b9875e4cd4fecb3cd37
Revathy Manulal <revathymanulal@hotmail.com>
2018-12-09 18:17:44 +0530

    Created scrape_itemprop.py
    Script which extracts itemprop properties from an HTML file

---
+++ scrape_itemprop.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get("https://www.myntra.com/dupatta/indya/indya-sea-green-solid-dupatta/5505196/buy")
+		
+		res.raise_for_status()
+		recipeSoup = BeautifulSoup(res.text, "html.parser")
+		type(recipeSoup)
+		instructions = recipeSoup.find_all(itemtype=re.compile(r"/Product$"))
+
+		# print 'length of instructions'+str(len(instructions))
+
+		product = {}
+
+
+		for product_values in range(len(instructions)):
+
+			if product_values==0:
+		
+				recipeSoup = BeautifulSoup(str(instructions[product_values]), "html.parser")
+				valueable_tags = recipeSoup.find_all(itemprop=re.compile(r"\w$"))
+
+
+				product = {}
+
+				for tag in valueable_tags:
+					contents = tag.contents
+					if '\n' in contents:
+						contents.remove('\n')
+					
+					print tag.get('itemprop'), tag.contents, contents
+					product[tag.get('itemprop')] = contents[0] if len(contents) >0 else ''
+
+			print product
